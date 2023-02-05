d14dada2c1e41dfebbae826bcb9a2c8e7b5e93de
Kang <rkdalsgur032@unist.ac.kr>
2023-02-05 15:44:09 +0900

    [#4] feature : TextTab 컴포넌트 제작

--- src/components/assembler/AssembleFilePanel.tsx
+++ src/components/assembler/AssembleFilePanel.tsx
@@ -1,7 +1,23 @@
+import { useRecoilValue } from "recoil";
+import { selectedAssemblyFileState } from "../../recoil/tab";
 import { AssembleFilePanelDisplay } from "../../styles/theme";
-
+import TextTab from "../TextTab";
+const datas = [
+  "example1.s",
+  "example2.s",
+  "example3.s",
+  "example4.s",
+  "example5.s",
+  "example6.s",
+  "example7.s",
+];
 const AssembleFilePanel = () => {
-  return <AssembleFilePanelDisplay>AssembleFilePanel</AssembleFilePanelDisplay>;
+  const selectedAssemblyFile = useRecoilValue(selectedAssemblyFileState);
+  return (
+    <AssembleFilePanelDisplay>
+      <TextTab title={selectedAssemblyFile} data={datas} />
+    </AssembleFilePanelDisplay>
+  );
 };
 
 export default AssembleFilePanel;
