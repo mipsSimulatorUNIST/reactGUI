3d700dee66b5b0feda1b89365307c4d7b14f4b64
Kang <rkdalsgur032@unist.ac.kr>
2023-02-05 15:16:40 +0900

    [#4] feature : file selector

--- src/components/assembler/FileSelctor.tsx
+++ src/components/assembler/FileSelctor.tsx
@@ -1,22 +1,10 @@
 import { FileSelctorDisplay } from "../../styles/theme";
-import { useEffect } from "react";
-
-async function getFileNames() {
-  try {
-    const response = await fetch("/sample_input/", {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const fileNames: string[] = await response.json();
-    console.log(fileNames);
-  } catch (error) {
-    console.error(error);
-  }
-}
+import { useRef } from "react";
+import { useRecoilState } from "recoil";
+import { selectedAssemblyFileState } from "../../recoil/tab";
 
 const FileSelctor = () => {
-  const assemblerFileList: string[] = [
+  const assemblerFileList = useRef<string[]>([
     "example1.s",
     "example2.s",
     "example3.s",
@@ -24,19 +12,36 @@ const FileSelctor = () => {
     "example5.s",
     "example6.s",
     "example7.s",
-  ];
+  ]);
 
-  useEffect(() => {
-    getFileNames();
-  }, []);
+  const [selectedAssemblyFile, setSelectedAssemblyFile] = useRecoilState(
+    selectedAssemblyFileState
+  );
+
+  const handleSelect = (evt: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const targetId = evt.currentTarget.id;
+    setSelectedAssemblyFile(targetId);
+  };
 
   return (
     <FileSelctorDisplay>
       <div>
         <div>sample_input</div>
         <div>
-          {assemblerFileList.map((ele, index) => {
-            return <div key={index}>{ele}</div>;
+          {assemblerFileList.current.map((ele, index) => {
+            return (
+              <div
+                id={ele}
+                key={index}
+                onClick={handleSelect}
+                style={{
+                  backgroundColor:
+                    selectedAssemblyFile === ele ? "red" : "grey",
+                }}
+              >
+                {ele}
+              </div>
+            );
           })}
         </div>
       </div>
--- src/recoil/tab.ts
+++ src/recoil/tab.ts
@@ -1,30 +1,6 @@
-import {atom} from "recoil";
+import { atom } from "recoil";
 
-export interface TabTypes {
-  id: number;
-  name: string;
-  isActive: boolean;
-}
-
-export const tabState = atom<TabTypes[]>({
-  key: "tabs",
-  default: [
-    {
-      id: 1,
-      name: "어셈블러",
-      isActive: true,
-    },
-
-    {
-      id: 2,
-      name: "시뮬레이터",
-      isActive: false,
-    },
-
-    {
-      id: 3,
-      name: "캐쉬",
-      isActive: false,
-    },
-  ],
+export const selectedAssemblyFileState = atom<string>({
+  key: "selectedAssemblyFileState",
+  default: "example1.s",
 });
