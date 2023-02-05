9c4700f16443e3afd07e6f93449dbd8af98ddec3
limetree06 <limetree06@unist.ac.kr>
2023-02-05 16:39:33 +0900

    [#4] feature : add color file

---
+++ src/styles/color.tsx
@@ -0,0 +1,6 @@
+export const BG = "#1E1E1E";
+export const GREY33 = "#333333";
+export const GREYD4 = "#D4D4D4";
+export const GREY85 = "#858585";
+export const GREYCC = "#cccccc";
+export const GREY37 = "#373737";
